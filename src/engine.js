46f1d270488b981b4cd1109239c65955a95e0434
LXXXY <xxt.qq.com>
2021-07-10 23:19:53 +0800

    背景绘制

--- src/engine.js
+++ src/engine.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 class Engine {
     constructor() {
@@ -9,12 +10,20 @@ class Engine {
         this._camera = null;
         
         this.init();
+
+        this._object = null;
+
+        this._animationFrame = null;
+
+        this._controls = null;
     }
 
     init() {
         this.createScene();
         this.createCamera();
         this.createRenderer();
+        this.createOrbitControl();
+        this.startRender();
     }
 
     createScene() {
@@ -25,7 +34,7 @@ class Engine {
         let width = window.innerWidth;
         let height = window.innerHeight;
         let wHRate = width / height;
-        let s = 500;
+        let s = 200;
         let camera = new THREE.OrthographicCamera(-s * wHRate, s * wHRate, s, -s, 1, 1000);
         camera.position.set(0, 0, 100);
         camera.lookAt(this._scene.position);
@@ -45,18 +54,51 @@ class Engine {
         renderer = null;
     }
 
+    createOrbitControl() {
+        this._controls = new OrbitControls( this._camera, this._renderer.domElement );
+    }
+
+    startRender() {
+        let renderFunc = (time) => {
+            this.render();
+            requestAnimationFrame(renderFunc);
+        }
+        renderFunc();
+    }
+
     render() {
         this._renderer.render(this._scene, this._camera);
     }
 
+    createObjects() {
+        
+        {
+            let textureLoader = new THREE.TextureLoader();
+            textureLoader.load("../img/earth.png", (texture) => {
+                texture.wrapS = THREE.RepeatWrapping;
+                let width = 1280 * 2;
+                texture.repeat.set(width / 64, 1);
+                let geometry = new THREE.BoxGeometry(width, 64, 50);
+                let material = new THREE.MeshBasicMaterial({
+                    map: texture
+                });
+                let mesh = new THREE.Mesh(geometry, material);
+                this._scene.add(mesh);
+            });
+        }
+
+    }
+
     start() {
-        let geometry = new THREE.BoxGeometry(100, 100, 100);
-        let mate = new THREE.MeshBasicMaterial({
-            color: 0x00ff00
-        });
-        let mesh = new THREE.Mesh(geometry, mate);
-        this._scene.add(mesh);
-        this.render();
+        // let geometry = new THREE.BoxGeometry(100, 100, 100);
+        // let mate = new THREE.MeshBasicMaterial({
+        //     color: 0x00ff00
+        // });
+        // let mesh = new THREE.Mesh(geometry, mate);
+        // this._scene.add(mesh);
+        
+
+        this.createObjects();
     }
 }
 
